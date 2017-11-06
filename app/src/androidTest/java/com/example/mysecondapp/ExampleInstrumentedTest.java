951ea87dd8b0841941d809e1ced42652f97e088a
krasig <krasig@gmail.com>
2017-11-07 00:48:45 +0200

    aaaa 2

--- app/src/androidTest/java/com/example/mysecondapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mysecondapp/ExampleInstrumentedTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/** proba 123
+/** proba 123 567
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
